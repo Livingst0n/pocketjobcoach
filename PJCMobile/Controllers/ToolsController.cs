ac12861651b17d8a6f6f6114d05e435df27e2b5e
Randy Hamblin <randyhamblin@mail.weber.edu>
2015-02-11 10:44:41 -0700

    Start of tools page

---
+++ PJCMobile/Controllers/ToolsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PJCMobile.Controllers
+{
+    public class ToolsController : Controller
+    {
+        //
+        // GET: /Tools/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Tools/Details/5
+
+        public ActionResult WageCalculator()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Tools/Create
+
+        public ActionResult Notes()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Tools/Create
+
+
+        public ActionResult Calendar()
+        {
+            return View();
+        }
+    }
+}
