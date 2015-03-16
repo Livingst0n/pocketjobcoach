0421e9b057857f9f83a2bd8b1c2d891ee91de291
Nathan Fiscus <nathan_fiscus@hotmail.com>
2015-03-15 23:52:40 -0600

    Role & Account Management

---
+++ PJCAdmin/Controllers/UnauthorizedController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PJCAdmin.Controllers
+{
+    public class UnauthorizedController : Controller
+    {
+        //
+        // GET: /Unauthorized/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
