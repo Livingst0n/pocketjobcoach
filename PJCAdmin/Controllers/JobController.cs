63d92cc2521a00f71202454762fc6a318bfcdd07
Chris Cotton <awolfnspirit@gmail.com>
2015-03-25 10:56:53 -0600

    More Job Stuff

--- PJCAdmin/Controllers/JobController.cs
+++ PJCAdmin/Controllers/JobController.cs
@@ -49,12 +49,21 @@ namespace PJCAdmin.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(job job, List<task> taskList)
+        public ActionResult Create(job job, string[] taskList)
         {
             if (ModelState.IsValid)
             {
                 
+                
+
+                foreach (var item in taskList)
+                {
+                    if(item != "false")
+                        job.tasks.Add(db.tasks.Find(Convert.ToInt32(item)));
+                }
+
                 db.jobs.Add(job);
+
                 db.SaveChanges();
 
                 return RedirectToAction("Index");
