c93330a6e8ca15e60c5de02c58ea274e9567ed05
Christopher Higgs <higgsch@yahoo.com>
2016-02-20 13:58:51 -0700

    Added WebAPI controller for testing serialization

---
+++ PJCAdmin/ControllersAPI/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PJCAdmin.Models;
+
+namespace PJCAdmin.ControllersAPI
+{
+    public class TestController : ApiController
+    {
+        private pjcEntities db = new pjcEntities();
+
+        public string Get()
+        {
+            return "";
+        }
+    }
+}
