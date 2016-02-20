387ccc23530a9cbec4c3c4655992ea43aa2ba023
Christopher Higgs <higgsch@yahoo.com>
2016-02-20 15:16:34 -0700

    Pulled out Routine helper code from RoutineController

---
+++ PJCAdmin/Classes/Helpers/APIModelHelpers/RoutineHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PJCAdmin.Models;
+
+namespace PJCAdmin.Classes.Helpers.APIModelHelpers
+{
+    public class RoutineHelper
+    {
+        private pjcEntities db = new pjcEntities();
+
+        public List<Routine> getRoutinesAssignedByParentForSerialization(string userName)
+        {
+            string parentUserName = db.UserNames.Find(userName).guardianUserName;
+
+            List<Routine> routinesByParent = new List<Routine>();
+
+            foreach (Routine r in db.UserNames.Find(userName).Routines1)
+            {
+                if (r.creatorUserName.Equals(parentUserName))
+                    routinesByParent.Add(ModelCopier.copyRoutine(r));
+            }
+
+            return routinesByParent;
+        }
+
+        public List<Routine> getRoutinesAssignedByJobCoachForSerialization(string userName)
+        {
+            string jobCoachUserName = db.UserNames.Find(userName).jobCoachUserName;
+
+            List<Routine> routinesByJobCoach = new List<Routine>();
+
+            foreach (Routine r in db.UserNames.Find(userName).Routines1)
+            {
+                if (r.creatorUserName.Equals(jobCoachUserName))
+                    routinesByJobCoach.Add(ModelCopier.copyRoutine(r));
+            }
+
+            return routinesByJobCoach;
+        }
+
+        public List<Routine> getAllRoutinesAssignedToUserForSerialization(string userName)
+        {
+            List<Routine> assignedRoutines = new List<Routine>();
+
+            foreach (Routine r in db.UserNames.Find(userName).Routines1)
+            {
+                assignedRoutines.Add(ModelCopier.copyRoutine(r));
+            }
+
+            return assignedRoutines;
+        }
+    }
+}
--- PJCAdmin/ControllersAPI/RoutineController.cs
+++ PJCAdmin/ControllersAPI/RoutineController.cs
@@ -6,27 +6,21 @@ using System.Net.Http;
 using System.Web.Http;
 using PJCAdmin.Models;
 using PJCAdmin.Classes;
-using PJCAdmin.Classes.APIModelHelpers;
+using PJCAdmin.Classes.Helpers.APIModelHelpers;
 
 namespace PJCAdmin.ControllersAPI
 {
     public class RoutineController : ApiController
     {
-        private pjcEntities db = new pjcEntities();
+        private RoutineHelper helper = new RoutineHelper();
 
         // GET api/Routine?token=<token>
         public IEnumerable<Routine> Get(string token)
         {
             APIAuth.authorizeToken(token);
             string userName = APIAuth.getUserNameFromToken(token);
-            List<Routine> assignedRoutines = new List<Routine>();
-
-            foreach (Routine r in db.UserNames.Find(userName).Routines1)
-            {
-                assignedRoutines.Add(ModelCopier.copyRoutine(r));
-            }
-
-            return assignedRoutines;
+            
+            return helper.getAllRoutinesAssignedToUserForSerialization(userName);
         }
 
         // GET api/Routine?token=<token>&assignedBy=<"Parent" or "Job Coach">
@@ -36,42 +30,12 @@ namespace PJCAdmin.ControllersAPI
             string userName = APIAuth.getUserNameFromToken(token);
 
             if (assignedBy.Equals("Parent"))
-                return getRoutinesAssignedByParent(userName);
+                return helper.getRoutinesAssignedByParentForSerialization(userName);
             if (assignedBy.Equals("Job Coach"))
-                return getRoutinesAssignedByJobCoach(userName);
+                return helper.getRoutinesAssignedByJobCoachForSerialization(userName);
 
             //assignedBy is not a valid string
             throw new HttpResponseException(new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest));
-        }
-
-        private List<Routine> getRoutinesAssignedByParent(string userName)
-        {
-            string parentUserName = db.UserNames.Find(userName).guardianUserName;
-
-            List<Routine> routinesByParent = new List<Routine>();
-
-            foreach (Routine r in db.UserNames.Find(userName).Routines1)
-            {
-                if (r.creatorUserName.Equals(parentUserName))
-                    routinesByParent.Add(r);
-            }
-
-            return routinesByParent;
-        }
-
-        private List<Routine> getRoutinesAssignedByJobCoach(string userName)
-        {
-            string jobCoachUserName = db.UserNames.Find(userName).jobCoachUserName;
-
-            List<Routine> routinesByJobCoach = new List<Routine>();
-
-            foreach (Routine r in db.UserNames.Find(userName).Routines1)
-            {
-                if (r.creatorUserName.Equals(jobCoachUserName))
-                    routinesByJobCoach.Add(r);
-            }
-
-            return routinesByJobCoach;
         }
     }
 }
